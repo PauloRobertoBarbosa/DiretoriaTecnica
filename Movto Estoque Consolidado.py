a111fa1341c92ae3e968eb2c65adfc76da2de73a
PauloRobertoBarbosa <paulo.r.o.barbosa@gmail.com>
2021-01-03 10:21:49 -0300

    Testing column name

--- Movto Estoque Consolidado.py
+++ Movto Estoque Consolidado.py
@@ -3,5 +3,9 @@ import os
 
 os.chdir(r'C:\Users\paulo.roberto\Documents\Movto Estoque Consolidado')
 
-MovtoEstoque = pd.read_excel('Movimento estoque consolidado - 2019.xlsx')
-print(MovtoEstoque.head(0))
+movestoque = pd.read_excel('Movimento estoque consolidado - 2019.xlsx')
+cols = list(movestoque.head(0))
+for i in cols:
+    print(i)
+
+
